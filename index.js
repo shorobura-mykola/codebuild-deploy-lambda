b0990421d55e19f87565a113a892a824b9a20047
mshorobura <shorobura.mykola@gmail.com>
2022-04-06 22:17:35 +0200

    update message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Something to test'),
+        body: JSON.stringify('Something to test + hello'),
     };
     return response;
 };
